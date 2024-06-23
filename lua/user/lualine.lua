8b67b27586bd3a30a58bd6093d97997dc297e2b4
Marko-Sanchez <sanchezmarco0909@gmail.com>
2024-06-22 17:41:43 -0700

    Explicit line_section declarations; added file size

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -1,6 +1,20 @@
+
 require('lualine').setup ({
-  options = { theme = 'auto' },
+  options = {
+      theme = 'auto',
+      component_separators = { left = '', right = '' },
+      section_separators = { left = '', right = '' },
+  },
   extensions = {
     'nvim-tree'
   },
+  sections = {
+    lualine_a = {{function() return '★' end, padding = {left = 0, right = 1} }, {'mode'} },
+    lualine_b = {{'branch'} , {'diff'}, {'diagnostics', sources = {'nvim_lsp'}} },
+    lualine_c = {{'filename'}},
+    lualine_x = {{'encoding'}, {'fileformat'}, {'filetype'} },
+    lualine_y = {{'progress'}, {'filesize'} },
+    lualine_z = {{'location'}, {function() return '★' end, padding = {left = 0, right = 1} } }
+
+  }
 })
